c09a3d567f35cfa32cbec53db2ee7a7fe3e1529a
cocoutbm <coco.durupt@gmail.com>
2023-03-29 00:17:43 +0200

    css + page de connexion (index)

--- index.php
+++ index.php
@@ -1,14 +1,54 @@
-<form action="./connection.php" method="post">
-		<div>Connexion à mon site de démo</div>
-		<div>
-			<label for="name">Login :</label>
-			<input autofocus type="text" id="mail" name="mail">
+<!DOCTYPE html>
+<html lang = "fr">
+<head>
+    <title>
+        Connexion au Compte
+    </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="blog.css">
+</head>
+
+<body class="body_inscription_connexion">
+	<div class="entete_inscription_connexion">
+
+		<!-- header de la page connexion -->
+		<!------------------------------------------------------------------------------------------->
+		<header>
+			<h1>Connexion</h1>
+		</header>
+  	</div>
+	<br>
+
+	<section class="formulaire_inscription_connexion">
+		<form action="./connection.php" method="post">
+			<div class="label">
+				<label for="name">Login :</label>
+			</div>
+			<div class="input">
+				<input autofocus type="text" id="mail" name="mail">
+			</div>
+
+			<div class="label">
+				<label for="password">Password :</label>
+			</div>
+			<div class="input">
+				<input type="password" id="password" name="password">
+			</div>
+
+			<div class="input">
+                <input type="submit" value="Se Connecter">
+            </div>
+
+			<!--<div>
+				<button type="submit">Se Connecter</button>
+			</div> -->
+		</form>
+
+		<div id="no_compte">
+			<p>Si vous n'avez pas de compte</p>
+			<a href="inscription.php">S'inscrire ici</a>
 		</div>
-		<div>
-			<label for="password">Password :</label>
-			<input type="password" id="password" name="password">
-		</div>
-		<div>
-			<button type="submit">Se Connecter</button>
-		</div>
-	</form>
+	</section>
+	<br>
+</body>
+</html>
--- inscription.php
+++ inscription.php
@@ -8,87 +8,88 @@
     <link rel="stylesheet" type="text/css" href="blog.css">
 </head>
 
-<body class="body_inscription">
-  <div class="entete_inscription">
-    <!-- header de la page inscription -->
+<body class="body_inscription_connexion">
+    <div class="entete_inscription_connexion">
+        <!-- header de la page inscription -->
+        <!------------------------------------------------------------------------------------------->
+        <header>
+        <h1>Inscription</h1>
+        </header>
+    </div><br>
+
+    <!-- section composée du formulaire d'inscription -->
     <!------------------------------------------------------------------------------------------->
-    <header>
-      <h1>Inscription</h1>
-    </header>
-  </div>
+    <section class="formulaire_inscription_connexion">
+        <form action="processinscription.php" method="POST">
+            <div class="label">
+                <label for="nom">Nom :</label>
+            </div>
+            <div class="input">
+                <input type="text" id="nom" name="nom" required><br>
+            </div>
 
-  <!-- section composée du formulaire d'inscription -->
-  <!------------------------------------------------------------------------------------------->
-  <section class="formulaire_inscription">
-    <form action="processinscription.php" method="POST">
-      <div class="label">
-        <label for="nom">Nom :</label>
-      </div>
-      <div class="input">
-        <input type="text" id="nom" name="nom" required><br>
-      </div>
+            <div class="label">
+                <label for="prenom">Prénom :</label>
+            </div>
+            <div class="input">
+                <input type="text" id="prenom" name="prenom" required><br>
+            </div>
 
-      <div class="label">
-        <label for="prenom">Prénom :</label>
-      </div>
-      <div class="input">
-        <input type="text" id="prenom" name="prenom" required><br>
-      </div>
+            <div class="label">
+                <label for="email">Adresse e-mail :</label>
+            </div>
+            <div class="input">
+                <input type="email" id="email" name="email" required><br>
+            </div>
 
-      <div class="label">
-        <label for="email">Adresse e-mail :</label>
-      </div>
-      <div class="input">
-        <input type="email" id="email" name="email" required><br>
-      </div>
+            <div class="label">
+                <label for="motdepasse">Mot de passe :</label>
+            </div>
+            <div class="input">
+                <input type="password" id="motdepasse" name="motdepasse" required><br>
+            </div>
 
-      <div class="label">
-        <label for="motdepasse">Mot de passe :</label>
-      </div>
-      <div class="input">
-        <input type="password" id="motdepasse" name="motdepasse" required><br>
-      </div>
+            <div class="label">
+                <label for="confmotdepasse">Confirmer le mot de passe :</label>
+            </div>
+            <div class="input">
+                <input type="password" id="confmotdepasse" name="confmotdepasse" required><br>
+            </div>
 
-      <div class="label">
-        <label for="confmotdepasse">Confirmer le mot de passe :</label>
-      </div>
-      <div class="input">
-        <input type="password" id="confmotdepasse" name="confmotdepasse" required><br>
-      </div>
+            <div class="label">
+                <label for="adresse">Adresse :</label>
+            </div>
+            <div class="input">
+                <input type="text" id="adresse" name="adresse"><br>
+            </div>
 
-      <div class="label">
-        <label for="adresse">Adresse :</label>
-      </div>
-      <div class="input">
-        <input type="text" id="adresse" name="adresse"><br>
-      </div>
+            <div class="label">
+                <label for="ville">Ville :</label>
+            </div>
+            <div class="input">
+                <input type="text" id="ville" name="ville"><br>
+            </div>
 
-      <div class="label">
-        <label for="ville">Ville :</label>
-      </div>
-      <div class="input">
-        <input type="text" id="ville" name="ville"><br>
-      </div>
+            <div class="label">
+                <label for="codepostal">Code postal :</label>
+            </div>
+            <div class="input">
+                <input type="text" id="codepostal" name="codepostal"><br>
+            </div>
 
-      <div class="label">
-        <label for="codepostal">Code postal :</label>
-      </div>
-      <div class="input">
-        <input type="text" id="codepostal" name="codepostal"><br>
-      </div>
+            <div class="label">
+                <label for="pays">Pays :</label>
+            </div>
+            <div class="input">
+                <input type="text" id="pays" name="pays"><br>
+            </div><br>
 
-      <div class="label">
-        <label for="pays">Pays :</label>
-      </div>
-      <div class="input">
-        <input type="text" id="pays" name="pays"><br>
-      </div><br>
-
-      <div class="input">
-        <input type="submit" value="S'inscrire">
-      </div>
-    </form>
-  </section>
+            <div class="input">
+                <input type="submit" value="S'inscrire">
+            </div>
+        </form>
+    </section>
+    <br>
 </body>
 
 </html>
