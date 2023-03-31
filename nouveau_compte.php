5bf6d4b2f8a837f4f6ba3bdeca192cb9ce958830
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2023-03-31 13:48:10 +0200

    affcihage des message d'erreur lors de mauvaise saisie de code

--- nouveau_compte.php
+++ nouveau_compte.php
@@ -3,18 +3,21 @@
 include("./fonctions_BDD.php");
 connect_db();
 
-include("./formulaire_inscription.php");
 
+include("./formulaire_inscription.php");
 $newAccountStatus = CheckNewAccountForm();
+
 
 if($newAccountStatus["Successful"]){
     $rootpath = "localhost/Projet_WE4A";
 	$redirect = "Location:http://".$rootpath."/formulaire_connection.php";
 	header($redirect);
 }else{
-	?>
-		<div class = "erreur"><?php echo($newAccountStatus["ErrorMessage"])?></div>
-	<?php
+	
+		echo '<h2 text-align: center; style="margin:auto; color:red; background-color: rgba(255, 255, 255, 0.6); width:40%;">'.$newAccountStatus["ErrorMessage"].'</h2>';
+	
 }
+
+
 ?>
 </html>
