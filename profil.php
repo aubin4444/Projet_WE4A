8191b6950a3df90929dacdac219c172e69f68665
cocoutbm <coco.durupt@gmail.com>
2023-04-02 11:21:36 +0200

    mdif photo + nom profil

--- profil.php
+++ profil.php
@@ -6,13 +6,35 @@
 	<title>Mon Profil</title>
 	<link rel="stylesheet" type="text/css" href="style.css"> 
 </head>
+
+<?php
+// Connection à la BDD
+include("./fonctions_BDD.php");
+connect_db();
+// Fichier permettant d'implémenter des animations JS
+include("./JavaScript/animation_simple.php");
+
+// Ouverture de la session afin de récupérer l'identifiant de l'utilisateur courant
+session_start();
+// Récupération du pseudo de l'utilisateur courant
+$query = "SELECT pseudo, photo_profil FROM `utilisateur` WHERE id = '".$_SESSION["userID"]."';";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+?>
+
 <body>
     <div id="entete_contenu_profil">
         <header>
             <div id="mon_profil">
-                <img src=".\images\paysage.jpg">
+                <!--affichage de la photo de profil de l'utilisateur courant-->
+                <img src=<?php echo($row["photo_profil"]); ?>>
                 <br/>
-                <span>Mon Profil</span>
+                <span>
+                    <?php
+                        // Affichage pseudo de l'utilisateur courant
+                        echo($row["pseudo"]);
+                    ?>
+                </span>
             </div>
             <div id="mes_postes">
                 <span>999</span>
