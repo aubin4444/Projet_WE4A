aa1e6418c3b9fdb1abd8680be39bd35f11fbbcbc
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2023-04-04 14:07:06 +0200

    affichage de tous les post dans profil

--- post_profil.php
+++ post_profil.php
@@ -1,45 +1,31 @@
 
 <?php
 // Récupération de l'id de l'auteur du post
-$query = "SELECT id_utilisateur FROM `post` WHERE id_utilisateur = '".$_SESSION['userID']."';";
+$query = "SELECT id_utilisateur, image, description FROM `post` WHERE id_utilisateur = '".$_SESSION['userID']."';";
 $result = $conn->query($query);
-$row = $result->fetch_assoc();
+//$row = $result->fetch_assoc();
 
+/*
 // Récupération du pseudo et de la photo de profil de l'utilisateur auteur du post
 $query = "SELECT pseudo, photo_profil FROM `utilisateur` WHERE id = '".$row["id_utilisateur"]."';";
-$result = $conn->query($query);
-$row = $result->fetch_assoc();
-?>
+$result1 = $conn->query($query);
+$row1 = $result1->fetch_assoc();
+*/
 
+while($row = $result->fetch_assoc()){
+
+?>
 <!--Publication-->
 <div id="poste_p">
     <!--Header de la publication-->
-	<div id = "poste_header">
-		<div class = "profil">
-			<div class="photo_profil">
-				<img src=<?php echo($row["photo_profil"]); ?>>
-			</div>
-			<div class="nom_profil">
-				<h2>
-                    <?php 
-                        // Affichage pseudo
-                        echo($row["pseudo"]);
-                    ?>
-                </h2>
-			</div>
-		</div>
-		<div class = "date_poste">
+	<div id = "poste_header_profil">
+		
+		<!--<div class = "date_poste">
 			<p>il y a 1 heure</p>
-		</div>
+		</div>-->
 	</div>
 
     <!--Image publiée-->
-	<?php
-	// Récupération de l'image et de la description du post
-	$query = "SELECT image, description FROM `post` WHERE id_utilisateur = '".$_SESSION['userID']."';";
-	$result = $conn->query($query);
-	$row = $result->fetch_assoc();
-	?>
 	<div id = "poste_image">
         <img src=<?php echo($row["image"]); ?>>
     </div>
@@ -56,3 +42,6 @@ $row = $result->fetch_assoc();
 	</div>
 </div>
 <br><br>
+<?php
+	}
+?>
