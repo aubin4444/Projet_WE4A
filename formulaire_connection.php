b55602db1cb2411b266b1ee34f0c834b5b65cbcb
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2023-04-07 23:43:15 +0200

    modification css

--- formulaire_connection.php
+++ formulaire_connection.php
@@ -23,17 +23,19 @@
 		<form action="#" method="post">
 			<div class="label">
 				<label for="name">Login :</label>
+				<div class="input">
+					<input autofocus type="text" id="mail" name="mail" required>
+				</div>
 			</div>
-			<div class="input">
-				<input autofocus type="text" id="mail" name="mail" required>
-			</div>
+			
 
 			<div class="label">
 				<label for="password">Password :</label>
+				<div class="input">
+					<input type="password" id="password" name="password" required>
+				</div>
 			</div>
-			<div class="input">
-				<input type="password" id="password" name="password" required>
-			</div>
+			<br><br>
 
 			<div class="input">
                 <input type="submit" value="Se Connecter">
@@ -42,7 +44,7 @@
 
 		<div id="no_compte">
 			<p>Si vous n'avez pas de compte</p>
-			<a href="nouveau_compte.php">S'inscrire ici</a>
+			<a href="nouveau_compte.php" style="color:#FFC226;">S'inscrire ici</a>
 		</div>
 	</section>
 	<br>
