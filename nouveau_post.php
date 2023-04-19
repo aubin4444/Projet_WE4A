fdcb6bc2fcc9be0c5c0c95b89f3bb3cae5bde224
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2023-04-19 19:34:04 +0200

    commit

--- nouveau_post.php
+++ nouveau_post.php
@@ -9,8 +9,10 @@
     
     // Insertion du formulaire dans lequel l'utilisateur crée un nouveau post
     include("./formulaire_post.php");
-    // Si le champs de l'image a été remplie
+    
+    // Si le champs de l'image a été remplie (nouveau post ou modification de l'image d'un post existant)
     if(isset($_FILES['file'])){
+        
         // Récupération du chemin temporaire de l'image
         $tmpName = $_FILES['file']['tmp_name'];
         // Récupération du nom de l'image
@@ -18,9 +20,8 @@
         // Récupération de la taille de l'image
         $size = $_FILES['file']['size'];
         $error = $_FILES['file']['error'];
-        $verif = false;
+
         if(is_uploaded_file($tmpName)){
-            
             // Récupération de nom du fichier sous forme de tableau en prenant "." comme séparateur
             $explodedName = explode('.', $name);
             // Récupération de l'extension de l'image (dernier élèment du tableau)
@@ -31,31 +32,20 @@
             $newName = "./images/photo_post/".$uniq.".".$extension;
             // Uploader l'image dans le dossier prévu
             move_uploaded_file($tmpName, $newName);
-
         } 
-    }elseif($id != -1){
-        echo("je passe la");
-        $newName = $_SESSION["image_prev"];
-    }
-    
-    if(isset($_POST['p_submit'])){
+
         // Insertion d'un nouveau post dans la BDD
+        // Si le post vient d'être créer 
         if($id == -1){
             $query = "INSERT INTO post (image, description, id_destination, id_utilisateur) VALUES ('".$newName."', '".$_POST["description"]."', 2, ".$_SESSION["userID"].")";
+        // Si le post existait déjà
         }else{
             $query = "UPDATE post SET image = '".$newName."', description = '".$_POST["description"]."', id_destination = 2, id_utilisateur = ".$_SESSION["userID"]." WHERE id = ".$id.";";
         }
+    
+        // Redirection vers la page profil
         if(mysqli_query($conn, $query)){
             header("Location:http://localhost/Projet_WE4A/profil.php?id=".$_SESSION["userID"]);
-            //echo($query);
         }
     }
-    
-            
-            
-        //}else{
-        //   echo("Erreur: Le fichier n'a pas été téléchargé correctement.");
-        //}
-    //}
-    
 ?>
