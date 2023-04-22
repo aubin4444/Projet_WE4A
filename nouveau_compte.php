bcd855d87608e75ede435e7af83c921e8e3f5e4f
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2023-04-22 22:56:17 +0200

    message de création de compte successful

--- nouveau_compte.php
+++ nouveau_compte.php
@@ -1,15 +1,24 @@
 <html>
+<a href = ./index.php>Se connecter</a>
 <?php
 include("./fonctions_BDD.php");
 connect_db();
 
 
-include("./formulaire_inscription.php");
+
 $newAccountStatus = CheckNewAccountForm();
+if(!$newAccountStatus["Successful"]){
+	include("./formulaire_inscription.php");
+}
 
 
 if($newAccountStatus["Successful"]){
-	header("Location:http://localhost/Projet_WE4A/index.php");
+	echo '<h2 text-align: center; style="margin:auto; color:green; background-color: rgba(255, 255, 255, 0.6); width:40%;">
+	Félicitation !!! Votre compte a bien été créé
+	<br>
+	<a href = ./index.php>Se connecter</a>
+	</h2>';
+	//header("Location:http://localhost/Projet_WE4A/index.php");
 }else{
 	
 		echo '<h2 text-align: center; style="margin:auto; color:red; background-color: rgba(255, 255, 255, 0.6); width:40%;">'.$newAccountStatus["ErrorMessage"].'</h2>';
