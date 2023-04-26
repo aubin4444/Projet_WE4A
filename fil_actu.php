47b1c1a406ffac9fcbc1fd085c7fa29d086464d3
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2023-04-26 17:29:29 +0200

    acceuil

--- fil_actu.php
+++ fil_actu.php
@@ -16,7 +16,7 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Page d'acceuil</title>
+	<title>Page d'accueil</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body >
